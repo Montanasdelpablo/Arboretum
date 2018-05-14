1792d0978d255f3c3fb247c396b3247b7ad8cab1
Pablo Cleij <pablocley@hotmail.com>
2018-05-14 11:50:59 +0200

    Added forgot password dialog to login

--- resources/assets/js/pages/web/login.vue
+++ resources/assets/js/pages/web/login.vue
@@ -3,39 +3,71 @@
         <v-parallax :src="image" height="700">
             <v-layout column align-center justify-center>
 
-            <v-card>
-                <v-card-media src="" height="200px">
-                </v-card-media>
-                <v-card-title primary-title>
-                  <div>
-                  <v-form v-model="valid">
-                            <v-text-field
-                              v-model="email"
-                              :rules="emailRules"
-                              :counter="10"
-                              label="Email"
-                              required
-                            ></v-text-field>
-                            <v-text-field
-                              v-model="password"
-                              :rules="passwordRules"
-                              label="Password"
-                              required
-                              invisible
-                            ></v-text-field>
 
-                            <v-btn
-                                 :disabled="!valid"
-                                 @click="submit"
-                               >
-                                 submit
-                              </v-btn>
-                            </v-form>
-                  </div>
-                </v-card-title>
+            <v-container style="min-height: 0; padding-top: 150px; background-color: #ffffff" :class="{'px-0': $vuetify.breakpoint.xsOnly }">
+            <v-flex xs12 sm6 offset-sm3>
+            <v-form v-model="valid">
+                      <v-text-field
+                        v-model="email"
+                        :rules="emailRules"
+                        :counter="10"
+                        label="Email"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="password"
+                        :rules="passwordRules"
+                        label="Password"
+                        required
+                        invisible
+                      ></v-text-field>
 
-            </v-card>
+                      <v-btn color="primary" dark @click.stop="forgotpassword = true">Wachtwoord vergeten</v-btn>
 
+
+                      <v-btn
+                           color="primary"
+                           style="float:right"
+                           :disabled="!valid"
+                           @click="submit"
+                         >
+                           submit
+                        </v-btn>
+                      </v-form>
+
+                      <v-dialog v-model="forgotpassword" max-width="500px">
+                            <v-card>
+                              <v-card-title>
+                                Wachtwoord vergeten
+                              </v-card-title>
+                              <v-card-text>
+                              <v-form v-model="valid">
+                                        <v-text-field
+                                          v-model="forgotEmail"
+                                          :rules="emailRules"
+                                          :counter="10"
+                                          label="Email"
+                                          required
+                                        ></v-text-field>
+
+                                        <v-btn
+                                             color="primary"
+                                             style="float:right"
+                                             :disabled="!valid"
+                                             @click="verstuur"
+                                           >
+                                             Verstuur wachtwoord
+                                          </v-btn>
+                                        </v-form>
+                              </v-card-text>
+                              <v-card-actions>
+                                <v-btn color="primary" flat @click.stop="forgotpassword=false">Sluiten</v-btn>
+
+                              </v-card-actions>
+                            </v-card>
+                          </v-dialog>
+                </v-flex>
+            </v-container>
 
 
             </v-layout>
@@ -65,6 +97,8 @@
           v => !!v || 'Password is required',
           v => v.length >= 3 || 'Password must be more than 3 characters'
         ],
+        forgotEmail: '',
+        forgotpassword: false,
 			}
 		},
 		methods: {
