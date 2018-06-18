4536f4672da9eab917c6474afa1b10fad8d125db
Pablo Cleij <pablocley@hotmail.com>
2018-06-18 23:00:37 +0200

    Added frontpage v1

--- resources/assets/js/pages/web/index.vue
+++ resources/assets/js/pages/web/index.vue
@@ -1,6 +1,156 @@
 <template>
     <v-container fluid>
 
+                  <v-parallax src="images/Geuren-Kleurenfeest_Notoarestoen.jpg" height="600">
+                    <v-layout
+                      column
+                      align-center
+                      justify-center
+                      class="white--text"
+                    >
+
+                      <h1 class="white--text mb-2 display-1 text-xs-center">Arboretum Eenrum</h1>
+
+                      <v-btn
+                        class="green lighten-2 mt-5"
+                        dark
+                        large
+                        href="/login"
+                      >
+                        Aanmelden
+                      </v-btn>
+                    </v-layout>
+                  </v-parallax>
+
+
+                  <section>
+                  <v-layout
+                    column
+                    wrap
+                    class="my-5"
+                    align-center
+                  >
+                    <v-flex xs12 sm4 class="my-3">
+                      <div class="text-xs-center">
+                         <h2 class="headline" > <strong> Ontdek onze planten collectie  </strong> </h2>
+                        <v-btn
+                          class="green lighten-2 mt-5"
+                          dark
+                          large
+                          href="/planten"
+                        >
+                        Naar plantenoverzicht
+                       </v-btn>
+                        <span class="subheading">
+
+                        </span>
+                      </div>
+                    </v-flex>
+                    <v-flex xs12>
+                      <v-container grid-list-xl>
+                        <v-layout row wrap align-center>
+                          <v-flex xs12 md4>
+                            <v-card class="elevation-0 transparent">
+                              <v-card-text class="text-xs">
+                                <v-icon x-large class="green--text text--lighten-2">local_florist</v-icon>
+                              </v-card-text>
+                              <v-card-title style="margin-left:2px" primary-title class="layout">
+                              <div class="headline text-xs-center">  <strong>Wat is een Arboretum? </strong></div>
+                              </v-card-title>
+                              <v-card-text>
+                              Een in bomen en struiken gespecialiseerde botanische tuin, in de 18e en 19e eeuw ontstaan om uitheems groen uit te proberen, met als doel het later in eigen land verder te verspreiden. Tegenwoordig is een arboretum behalve een verzameling groen ook vaak een centrum van wetenschappelijk onderzoek en voorlichting.
+                              </v-card-text>
+                            </v-card>
+                          </v-flex>
+                          <v-flex xs12 md4>
+                            <v-card class="elevation-0 transparent">
+                              <v-card-text class="text-xs">
+                                <v-icon x-large class="green--text text--lighten-2">tag_faces</v-icon>
+                              </v-card-text>
+                              <v-card-title  style="margin-left:2px" primary-title class="layout">
+                                <div class="headline"> <strong> Het groene levenswerk van notaris Smit </strong></div>
+                              </v-card-title>
+                              <v-card-text>
+                                Het arboretum Notoarestoen in Eenrum bevat een internationale plantencollectie met de Japanse tempelboom, Chinese vaantjesboom, Amerikaanse Sequoia''s, rododendrons en nog veel meer. De tuin is het levenswerk van notaris Smit uit Eenrum, vandaar de naam ''Notoarestoen'': Gronings voor ''tuin van de notaris''.
+
+Notoarestoen is dagelijks vrij toegankelijk
+                              </v-card-text>
+                            </v-card>
+                          </v-flex>
+                          <v-flex xs12 md4>
+                            <v-card class="elevation-0 transparent">
+                              <v-card-text class="text-xs">
+                                <v-icon x-large class="green--text text--lighten-2">wb_sunny</v-icon>
+                              </v-card-text>
+                              <v-card-title style="margin-left:2px" primary-title class="layout">
+                                <div class="headline text-xs-center"> <strong> Groene Inspiratie </strong> </div>
+                              </v-card-title>
+                              <v-card-text>
+                                250 informatiebordjes laten u met nieuwe ogen kijken naar de exotische variëteiten van de natuur. Met informatie over de historie en bijzonderheden van de rododendron, de sequoia en andere uitheemse soorten.
+
+Een klimaatexpositie inspireert tot groene keuzes en laat de effecten van de klimaatverandering zien. Het duurzame tuinhuis in Notoarestoen is voorzien van zonnepanelen en een milieuvriendelijk toiletsysteem.
+                              </v-card-text>
+                            </v-card>
+                          </v-flex>
+                        </v-layout>
+                      </v-container>
+                    </v-flex>
+                  </v-layout>
+                  </section>
+
+                  <section>
+                  <v-jumbotron src="images/notaris_smit_1968.jpg" >
+                    <v-layout column align-center justify-center>
+
+                    </v-layout>
+                  </v-jumbotron>
+                  </section>
+
+                  <section>
+                  <v-container grid-list-xl>
+                    <v-layout row wrap justify-center class="my-5">
+                      <v-flex xs12 sm4>
+                        <v-card class="elevation-0 transparent">
+                          <v-card-title primary-title class="layout">
+                            <div class="headline" style="margin-left:12px"><strong> Dagelijks geopend </strong></div>
+                          </v-card-title>
+                          <v-card-text>
+                                Notoarestoen is gevestigd aan het Ernstheemsterpad in Eenrum. <br /> <br />
+
+                                <strong> Met de auto </strong> <br />
+                                Komende vanuit Winsum, Mensingeweer, in Eenrum 2e straat rechts (Spoorlaan).
+                                Aan het eind van de Spoorlaan bevindt zich aan uw rechterhand het arboretum.
+                                <br /> <br />
+                                <strong> Met het openbaar vervoer </strong> <br />
+                                Vanaf Groningen de trein naar Winsum. Vanaf Winsum bus 65 naar Zoutkamp.
+                                Uitstappen bij halte De Vennen in Eenrum. Daarna is het nog enkele minuten lopen.
+                          </v-card-text>
+                        </v-card>
+                      </v-flex>
+                      <v-flex xs12 sm4 offset-sm1>
+                        <v-card class="elevation-0 transparent">
+
+                          <v-card-text>
+              <div style="width: 100%"><iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%&amp;height=300&amp;hl=en&amp;coord=53.361222, 6.465152&amp;q=Ernstheemsterpad%2C%209967%20PJ%20Eenrum%2C%20Nederland+(Arboretum%20Eenrum)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/create-google-map/"></a></iframe></div><br />
+                          </v-card-text>
+
+                        </v-card>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                  </section>
+
+                  <v-footer class="green lighten-1" style="background-color: #78b856">
+                  <v-layout row wrap align-center  >
+                    <v-flex xs12>
+                      <div class="white--text ml-3">
+                        Gemaakt door
+                         <a class="white--text" href="https://vuetifyjs.com" target="_blank">Derkjan Super</a>
+                        & <a class="white--text" href="https://github.com/vwxyzjn">Pablo Cleij</a>
+                      </div>
+                    </v-flex>
+                  </v-layout>
+                  </v-footer>
     </v-container>
 </template>
 
